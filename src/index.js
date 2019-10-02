9115c3600979ff22c6dd28cabaed4a6fcb12e5c6
Stadionova <726383@mail.ru>
2019-10-02 13:42:42 +0300

    little pixels changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./styles.less";
 import "normalize.css";
+
+// .map {
+//     border: 20px solid #ffffff;
+//     .map-yandex {
+//         position: absolute;
+//         width: 100%;
+//     }
+
+//     // за счёт медиа делается мобильная вёрстка
+//     // блок перескакивает ниже при уменьшении размера экрана
+//     // не при уменьшении масштаба
+//     @media (max-width: 768px){
+//         .map-yandex {
+//             position: static;
+//         }
+//     }
