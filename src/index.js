7f4a438acf8309249e3f19b99fcbdc00ff22872c
Stadionova <726383@mail.ru>
2019-10-02 19:54:58 +0300

    layout correcred

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import "./styles.less";
 import "normalize.css";
 
-// .map {
-//     border: 20px solid #ffffff;
-//     .map-yandex {
-//         position: absolute;
-//         width: 100%;
-//     }
+function hideMapInfo(mouseEvent) {
+    insideMapElem.style.display = "none";
+}
 
-//     // за счёт медиа делается мобильная вёрстка
-//     // блок перескакивает ниже при уменьшении размера экрана
-//     // не при уменьшении масштаба
-//     @media (max-width: 768px){
-//         .map-yandex {
-//             position: static;
-//         }
-//     }
+function init() {
+    var insideMapElem = document.getElementById("mapInfo");
+    var closeButton = document.getElementById("cross");
+    closeButton.onclick = hideMapInfo;
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    init();
+});
+
+// function showModal(mouseEvent) {
+//   modal.style.display = "block";
+// }
+
+// function hideModal(mouseEvent) {
+//   modal.style.display = "none";
+// }
+
+// function init() {
+//   console.log("init");
+
+//   modal = document.getElementById("modal");
+//   modalClose = document.getElementById("modal-close");
+//   bookingBtn = document.getElementById("booking-btn");
+
+//   modalClose.onclick = hideModal;
+//   bookingBtn.onclick = showModal;
+// }
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   console.log("DOMContentLoaded");
+//   init();
+// });
