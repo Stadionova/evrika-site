951a3492dd01665e35e0cfdbe379e55cb4e6fa20
Stadionova <726383@mail.ru>
2019-10-04 10:42:01 +0300

    header bem & nesting done

--- src/index.js
+++ src/index.js
@@ -17,27 +17,3 @@ function init() {
 document.addEventListener("DOMContentLoaded", function (event) {
     init();
 });
-
-// function showModal(mouseEvent) {
-//   modal.style.display = "block";
-// }
-
-// function hideModal(mouseEvent) {
-//   modal.style.display = "none";
-// }
-
-// function init() {
-//   console.log("init");
-
-//   modal = document.getElementById("modal");
-//   modalClose = document.getElementById("modal-close");
-//   bookingBtn = document.getElementById("booking-btn");
-
-//   modalClose.onclick = hideModal;
-//   bookingBtn.onclick = showModal;
-// }
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   console.log("DOMContentLoaded");
-//   init();
-// });
